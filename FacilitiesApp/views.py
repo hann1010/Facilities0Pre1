584e625919c5f714a2e6c2b08aee3f67bd87c8c6
hann1010 <hanna.paivasto@gmail.com>
2020-02-17 21:40:19 +0200

    add userprofile

--- FacilitiesApp/views.py
+++ FacilitiesApp/views.py
@@ -4,6 +4,8 @@ import datetime
 def home(request):
     x = datetime.datetime.now()
     date_tmp = (x.strftime('%d-%m-%Y, %H:%M:%S'))
+    
+ 
 #    current_user = request.user
 #    disp_temp = date_tmp + " User: " + str(current_user)
     return render(request, "FacilitiesApp/index.html", {"date_str": date_tmp})
