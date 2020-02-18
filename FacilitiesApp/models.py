631574213f5371c067b83c7d47e9fc1fb9e89d90
hann1010 <hanna.paivasto@gmail.com>
2020-02-18 21:57:54 +0200

    add db table

--- FacilitiesApp/models.py
+++ FacilitiesApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Apartment(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, blank=True)
+    phone_no = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100)
+    house_company = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address + " " + self.last_name
+
+#def get_absolute_url(self):
+#        return reverse('post-detail', kwargs={'pk': self.pk})
