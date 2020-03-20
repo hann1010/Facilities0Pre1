14319dde36c893ba09e1370bbd85261f35eb19f3
hann1010 <hanna.paivasto@gmail.com>
2020-03-20 21:17:43 +0200

    Adding Database Table

--- FacilitiesApp/admin.py
+++ FacilitiesApp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Apartment
+from .models import Ticket
 
 admin.site.register(Apartment)
+admin.site.register(Ticket)
--- FacilitiesApp/models.py
+++ FacilitiesApp/models.py
@@ -21,5 +21,25 @@ class Apartment(models.Model):
     def __str__(self):
         return self.address + " " + self.last_name
 
+class Ticket(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, blank=True)
+    phone_no = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100)
+    house_company = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    repair_state = models.CharField(max_length=100)
+    repair = models.CharField(max_length=100)
+    date_repair = models.DateTimeField(max_length=100)
+    notes = models.TextField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    date_last_save = models.DateTimeField(auto_now=timezone.now)
+    username_first = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.address + " " + self.last_name + " " + self.repair
+
 def get_absolute_url(self):
         return reverse('apartment-detail', kwargs={'pk': self.pk})
