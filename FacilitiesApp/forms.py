919933293f18c77e00697809fbf44af93237d239
hann1010 <hanna.paivasto@gmail.com>
2020-04-16 21:53:38 +0300

    Fixing forms.py

--- FacilitiesApp/forms.py
+++ FacilitiesApp/forms.py
@@ -6,12 +6,12 @@ from .models import Ticket
 
 YEAR_CHOICES = ['2019', '2020', '2021', '2022', '2023', '2024']
 
-CHOICES = (
-            (0, "New"),
-            (1, "Received"),
-            (2, "Waiting someting"),
-            (3, "Repair done")
-           )
+REPAIR_CHOICES = (
+                (0, "New"),
+                (1, "Received"),
+                (2, "Waiting someting"),
+                (3, "Repair done")
+                )
 
 class State_select(forms.NullBooleanSelect):
     input_type = 'text'
@@ -24,7 +24,7 @@ class TicketForm(ModelForm):
         fields = ['first_name', 'last_name', 'address', 'received', 'title', 'fault','repair_state','repair', 'date_repair', 'phone_no', 'email', 'house_company', 'notes']
         widgets = {
             'date_repair': forms.SelectDateWidget(years=YEAR_CHOICES),
-            'repair_state': forms.Select(choices = CHOICES),
+            'repair_state': forms.Select(choices = REPAIR_CHOICES),
             'received': State_select(),           
         }
 
