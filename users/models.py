383d5ad7ea2e85bf991d30d525d064d4d3dfdf46
hann1010 <hanna.paivasto@gmail.com>
2021-01-11 19:56:35 +0200

    add list_rows to db

--- users/models.py
+++ users/models.py
@@ -7,9 +7,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     members_of = models.CharField(max_length=200, blank=True)
     user_level= models.IntegerField(default=1)
+    list_rows= models.IntegerField(default=10)
     
     def __str__(self):
-        return  str(self.user) + " / " + str(self.user_level) + " / " + self.members_of
+        return  str(self.user) + " / " + str(self.user_level) + " / " \
+            + self.members_of + " / " + str(self.list_rows)
 
 
 @receiver(post_save, sender=User)
