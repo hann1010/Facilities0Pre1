b7b3077008a9bd5e4c3159f4e314e2690864cecd
hann1010 <hanna.paivasto@gmail.com>
2021-01-18 19:21:53 +0200

    add list_rows to profile view

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -18,3 +18,7 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
 
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['list_rows']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -22,7 +22,8 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        
+        p_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+
         if u_form.is_valid():
             u_form.save()
             
@@ -31,9 +32,13 @@ def profile(request):
 
     else:
         u_form = UserUpdateForm(instance=request.user)
-        
-
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'form': u_form,
+        'p_form': p_form
+    }
     
 
-    return render(request, 'users/profile.html', {'form': u_form})
+    return render(request, 'users/profile.html', context)
 
