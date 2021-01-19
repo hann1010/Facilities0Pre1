6255fc6a4ccbf088c187e480442384e29970bd26
hann1010 <hanna.paivasto@gmail.com>
2021-01-19 19:51:56 +0200

    add list_rows to profile view

--- users/views.py
+++ users/views.py
@@ -24,9 +24,9 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
 
-        if u_form.is_valid():
+        if u_form.is_valid() and p_form.is_valid():
             u_form.save()
-            
+            p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('home')
 
